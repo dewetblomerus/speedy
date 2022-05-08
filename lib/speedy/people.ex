244aeb2d06a58ba5052b163fb60a32f79d1759f3
De Wet Blomerus <dewet.blomerus@salesloft.com>
2022-05-07 21:54:21 -0400

    just a list

--- lib/speedy/people.ex
+++ lib/speedy/people.ex
@@ -2,6 +2,6 @@ defmodule Speedy.People do
   alias Speedy.Person
 
   def generate(amount) do
-    Stream.map(1..amount, &Person.generate(&1))
+    Enum.map(1..amount, &Person.generate(&1))
   end
 end
--- test/speedy/people_test.exs
+++ test/speedy/people_test.exs
@@ -5,12 +5,11 @@ defmodule Speedy.PeopleTest do
 
   describe "generate/1" do
     test "generates a stream of people" do
-      assert [%Person{}] = People.generate(1) |> Enum.map(& &1)
+      assert [%Person{}] = People.generate(1)
 
       amount = Enum.random(1..1000)
 
       assert People.generate(amount)
-             |> Enum.map(& &1)
              |> length() == amount
     end
   end
