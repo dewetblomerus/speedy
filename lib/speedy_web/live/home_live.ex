ad1fa9c7542f71b41a08e1812305e5ffff73cfcc
De Wet Blomerus <dewet.blomerus@salesloft.com>
2022-05-09 18:30:03 -0400

    number input 5️⃣

--- lib/speedy_web/live/home_live.ex
+++ lib/speedy_web/live/home_live.ex
@@ -1,14 +1,29 @@
 defmodule SpeedyWeb.HomeLive do
   use SpeedyWeb, :live_view
+  alias Speedy.People
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    initial_amount = 5
+
+    {:ok,
+     assign(
+       socket,
+       amount: initial_amount,
+       people: People.generate(initial_amount)
+     )}
   end
 
-  def render(assigns) do
-    ~H"""
-    <h1>Hello!!!</h1>
-    """
+  @impl Phoenix.LiveView
+  def handle_event("update", %{"amount" => input_amount}, socket) do
+    IO.inspect(socket, label: "🔌")
+    amount = String.to_integer(input_amount)
+
+    {:noreply,
+     assign(
+       socket,
+       amount: amount,
+       people: People.generate(amount)
+     )}
   end
 end
