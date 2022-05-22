b51e62e4eb5adde26fc3bbd40fe3b30c14dad05b
De Wet Blomerus <dewet.blomerus@salesloft.com>
2022-05-22 09:37:55 -0400

    it is just an example app

--- lib/speedy_web/router.ex
+++ lib/speedy_web/router.ex
@@ -14,32 +14,13 @@ defmodule SpeedyWeb.Router do
     plug :accepts, ["json"]
   end
 
+  import Phoenix.LiveDashboard.Router
+
   scope "/", SpeedyWeb do
     pipe_through :browser
 
     live "/", HomeLive
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", SpeedyWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: SpeedyWeb.Telemetry
-    end
+    live_dashboard "/dashboard", metrics: SpeedyWeb.Telemetry
   end
 
   # Enables the Swoosh mailbox preview in development.
